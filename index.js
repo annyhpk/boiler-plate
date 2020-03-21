057f99a524c6f524726e5dbe72045f0fd1692178
Chanho Kim <cksgh063@gmail.com>
2020-03-21 16:25:47 +0900

    boiler-plate

---
+++ index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connnected...'))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send('Hello World'));
+
+app.post("/register", (req, res) => {
+  
+
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    });
+  });
+
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
