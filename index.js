c4a670c196c1a8681c6110f0e0a078fe981216ab
Chanho Kim <cksgh063@gmail.com>
2020-03-21 18:15:47 +0900

    Add jwt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { User } = require('./models/User');
 
 const config = require('./config/key');
@@ -19,7 +20,6 @@ mongoose.connect(config.mongoURI, {
 app.get("/", (req, res) => res.send('Hello World'));
 
 app.post("/register", (req, res) => {
-  
   const user = new User(req.body);
 
   user.save((err, doc) => {
@@ -28,7 +28,42 @@ app.post("/register", (req, res) => {
       success: true
     });
   });
+});
 
+app.post("/login", (req,res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      return res.json({
+        loginSuccess: false,
+        message: "에러 발생",
+        err
+      });
+    } 
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      });
+    } 
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ 
+          loginSuccess: false, 
+          message: "비밀번호가 틀렸습니다."
+        });
+      }
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+          
+        return res.cookie("X_auth", user.token)
+                  .status(200)
+                  .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+
+  });
 });
 
 
