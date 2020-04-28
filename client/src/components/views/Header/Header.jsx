8f586103ff706892912752e6e3d24056604b75df
Chanho Kim <cksgh063@gmail.com>
2020-04-28 23:12:59 +0900

    Update

--- client/src/components/views/Header/Header.jsx
+++ client/src/components/views/Header/Header.jsx
@@ -1,40 +1,62 @@
-import React from 'react';
-import axios from 'axios';
-import { Layout, Button } from 'antd';
-import styled from 'styled-components';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import axios from "axios";
+import { Layout, Button } from "antd";
+import styled from "styled-components";
+import { withRouter } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const StyledHeader = styled(Layout.Header)`
-    display: flex;
-    justify-content: flex-end;
-    padding: 0;
-`
+  display: flex;
+  justify-content: flex-end;
+  padding: 0;
+`;
 
 const StyledButton = styled(Button)`
-    margin-top: 1.2em;
-    margin-right: 1em;
-` 
-    
+  margin-top: 1.2em;
+  margin-right: 1em;
+`;
+
 function Header(props) {
+  const user = useSelector(state => state.user);
 
-    const onClickHandler = () => {
-        axios.get('/api/users/logout')
-            .then(response => {
-                if(response.data.success) {
-                    props.history.push('/login');
-                } else {
-                    alert('Failed to logout');
-                }
-            });
-    };
+  const onClickLogout = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to logout");
+      }
+    });
+  };
 
+  const onClickLogin = () => {
+    props.history.push("/login");
+  }
+
+  const onClickRegister = () => {
+    props.history.push("/register");
+  }
+
+  if (user.userData && !user.userData.isAuth) {
     return (
-        <StyledHeader className="site-layout-background">
-            <StyledButton danger onClick={onClickHandler}>
-                logout
-            </StyledButton>
-        </StyledHeader>
-    )
-};
+      <StyledHeader className="site-layout-background">
+        <StyledButton danger onClick={onClickLogout}>
+          Logout
+        </StyledButton>
+      </StyledHeader>
+    );
+  } else {
+    return (
+      <StyledHeader className="site-layout-background">
+        <StyledButton type="primary" onClick={onClickRegister}>
+          Signup
+        </StyledButton>
+        <StyledButton type="primary" onClick={onClickLogin}>
+          Login
+        </StyledButton>
+      </StyledHeader>
+    );
+  }
+}
 
 export default withRouter(Header);
