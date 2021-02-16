236e1906790f4eb2bc613ec581ddeaa8a88c3840
Chanho Kim <cksgh063@gmail.com>
2021-02-16 12:29:10 +0900

    Change targets-browsers

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
                 presets: [
                    ['@babel/preset-env', {
                     targets: {
-                        browsers: ['> 5% in KR'],
+                        browsers: ['last 2 versions', 'ie >= 11'],
                     },
                     debug: true,
                    }],
